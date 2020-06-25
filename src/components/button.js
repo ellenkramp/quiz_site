c18de9eb42bb4b8c7a8dc11ee469921cda807c47
Ellen Kramp <ellenkramp@gmail.com>
2020-06-25 10:56:23 -0400

    refactor button

--- src/components/button.js
+++ src/components/button.js
@@ -1,23 +1,24 @@
-import React from "react";
-import { ButtonStyled } from '../../styles';
+import React from "react"
+import { ButtonStyled } from "../../styles"
 
 export default ({ props }) => {
-  const { handleNext, handleSubmit, currentIndex, totalQuestions } = props;
-  let Button;
-  const lastQuestion = (currentIndex === (totalQuestions - 1));
+  const { handleNext, handleSubmit, currentIndex, totalQuestions } = props
+  let Button
+  const lastQuestion = currentIndex === totalQuestions - 1
 
   if (!lastQuestion) {
     Button = () => {
       return (
-        <ButtonStyled variant="text" onClick={handleNext}>Next -></ButtonStyled>
+        <ButtonStyled variant="text" onClick={handleNext}>
+          Next -{">"}
+        </ButtonStyled>
       )
-    };
+    }
   }
   if (lastQuestion) {
     Button = () => (
       <ButtonStyled onClick={handleSubmit}>See My Results!</ButtonStyled>
-    );
+    )
   }
   return <Button />
-
-};
+}
